356c824237dce164bfac118c83950711704e1b3f
Anton Chuv <73758803+Anton-Chuv@users.noreply.github.com>
2025-08-06 09:06:06 +0400

    prev solution doesn't work this time LC hint to use a segment tree. I will return when read more about it

---
+++ src/2025/08/2025-08-06.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int count = 0;
+
+        return count;
+    }
+};
+
+int main() {
+    cout << "hi" << endl;
+
+    return 0;
+}
