cbf90eea4c31e2b6250a7f16e9b8631b20a2ad3e
Anton Chuv <73758803+Anton-Chuv@users.noreply.github.com>
2025-08-06 21:14:09 +0400

    Update 2025-08-06.cpp

--- src/2025/08/2025-08-06.cpp
+++ src/2025/08/2025-08-06.cpp
@@ -3,16 +3,23 @@
 using namespace std;
 
 class Solution {
+
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int count = 0;
+
 
         return count;
     }
 };
 
 int main() {
-    cout << "hi" << endl;
+    // cout << "hi" << endl;
+    vector<int> f = {4,2,5};
+    vector<int> b = {3,5,4};
+    Solution sol;
+
+    cout << sol.numOfUnplacedFruits(f, b);
 
     return 0;
 }
