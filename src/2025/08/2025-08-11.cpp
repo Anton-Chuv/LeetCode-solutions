0611ae4a1bf82848f2e5b64d3c635ac2ca3576e6
Anton Chuv <73758803+Anton-Chuv@users.noreply.github.com>
2025-08-13 11:43:38 +0400

    Was beasy

---
+++ src/2025/08/2025-08-11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int> out;
+
+        return out;
+    }
+};
+
+int main() {
+    Solution solution;
+    int n = 2;
+    vector<vector<int>> queries = {};
+    for (int x: solution.productQueries(n, queries))
+        cout << x << ' ';
+    return 0;
+}
