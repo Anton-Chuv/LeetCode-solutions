3c66368949e290c8098daf2b58aff204774c1301
Anton Chuv <73758803+Anton-Chuv@users.noreply.github.com>
2025-08-13 11:55:19 +0400

    Create 2025-08-13.cpp

---
+++ src/2025/08/2025-08-13.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n < 1)
+            return false;
+        if (n == 1)
+            return true;
+        while (n % 3 == 0) {
+            n /= 3;
+            if (n == 1)
+                return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution solution;
+    return solution.isPowerOfThree(45);
+}
