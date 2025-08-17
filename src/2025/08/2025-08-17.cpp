95543b2818a4f9a192a4fcc694d3fc9643075e44
Anton Chuv <73758803+Anton-Chuv@users.noreply.github.com>
2025-08-17 13:06:19 +0400

    оптимизировал алгоритм, поправил код

--- src/2025/08/2025-08-17.cpp
+++ src/2025/08/2025-08-17.cpp
@@ -1,11 +1,11 @@
-#include <algorithm>
-#include <iostream>
 #include <vector>
 
 using namespace std;
+
 class Solution {
 public:
     double new21Game(int n, int k, int maxPts) {
+        double sum = 0;
         if (k == 0)
             return 1.0;
         vector<double> p(maxPts, 0);
@@ -13,10 +13,10 @@ public:
             double q = 0; // prob of get exact i score on any taken card within i card taken
             if (i <= maxPts) // take score with one card
                 q = 1.0 / maxPts;
-            for (int j = 0; j < maxPts; j++) {
-                q += p[j] / maxPts;
-            }
+            q += sum / maxPts;
+
             p.push_back(q);
+            sum += q - p[0];
             p.erase(p.begin());
         }
         double ans = 0;
@@ -24,48 +24,14 @@ public:
             double q = 0; // prob of get exact i score on any taken card within i card taken
             if (i <= maxPts) // take score with one card
                 q = 1.0 / maxPts;
-            for (int j = 0; j < maxPts - (i - k); j++)
-                q += p[maxPts - j - 1] / maxPts;
+            q += sum / maxPts;
+            if (p.size() > 0) {
+                sum -= p[0];
+                p.erase(p.begin());
+            }
             ans += q;
         }
 
         return ans;
     }
 };
-
-int main() {
-    Solution sol;
-    cout << sol.new21Game(6, 1, 10);
-    return 0;
-}
-
-double new21Game(int n, int k, int maxPts) {
-    double *p = (double *)malloc(maxPts * sizeof(double));
-    for (int i = 0; i < maxPts; i++) p[i] = 0.0;
-
-    if (k == 0)
-        return 1.0;
-
-    for (int i = 1; i < k; i++) {
-        double q = 0; // prob of get exact i score on any taken card within i card taken
-        if (i <= maxPts) // take score with one card
-            q = 1.0 / maxPts;
-        q += p[0] / maxPts;
-        for (int j = 1; j < maxPts; j++) {
-            q += p[j] / maxPts;
-            p[j - 1] = p[j];
-        }
-        p[maxPts - 1] = q;
-    }
-    double ans = 0;
-    for (int i = k; i <= n; i++) {
-        double q = 0; // prob of get exact i score on any taken card within i card taken
-        if (i <= maxPts) // take score with one card
-            q = 1.0 / maxPts;
-        for (int j = 0; j < maxPts - (i - k); j++)
-            q += p[maxPts - j - 1] / maxPts;
-        ans += q;
-    }
-
-    return ans;
-}
