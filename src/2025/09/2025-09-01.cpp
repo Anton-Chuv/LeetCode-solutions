111bb789b2c94904be795c7fa05979cbe36432d6
Anton Chuv <73758803+Anton-Chuv@users.noreply.github.com>
2025-09-02 09:10:20 +0400

    Add file for max average ratio (without solution yet)

---
+++ src/2025/09/2025-09-01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        double maxRatio = 0.0;
+
+        return maxRatio;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<vector<int>> classes = {{1,2},{3,5},{2,2}};
+    int extraStudents = 2;
+    cout << solution.maxAverageRatio(classes, extraStudents) << endl;
+    return 0;
+}
